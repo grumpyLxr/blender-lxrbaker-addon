930d1474b691cb65334eeba4a25fa0f2de66d48f
grumpyLxr <ludger.steens@gmail.com>
2025-08-11 20:40:50 +0200

    Extend package.sh to add add-on version to the zip file name.

--- package.sh
+++ package.sh
@@ -3,4 +3,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-zip BlenderLxrBakerAddon BlenderLxrBakerAddon/*.py
+bl_info="$(head -n 10 BlenderLxrBakerAddon/__init__.py)"
+version="$(python/bin/python3 -c "${bl_info}; print('.'.join(map(str, bl_info['version'])))")"
+
+zip "BlenderLxrBakerAddon-${version}.zip" BlenderLxrBakerAddon/*.py
